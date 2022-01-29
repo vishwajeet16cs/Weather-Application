f452f3a7d9e386977a2b758a5faa9ca7e9821dd0
Vishwajeet Kumar <vishwajeet@germantowntech.in>
2022-01-29 11:15:01 +0530

    Ready to Deploy

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const forcast = require("./Utils/forcast")
 const geocode =require("./Utils/geocode")
 
 const app=express()
+const port=process.env.PORT || 3000
 
 //define paths for Express config
 const publicDirectoryPath=path.join(__dirname,'../public')
@@ -113,6 +114,6 @@ app.get('*',(req,res)=>{
 })
 
 
-app.listen(3000,()=>{
-    console.log("server is running on port no 3000")
+app.listen(port,()=>{
+    console.log(`server is running on port no ${port}`)
 })
